654b1b563646204ed60f792795ac300ef78c2fb0
sreedev <sreedevpadmakumar@gmail.com>
2020-12-23 18:52:40 +0530

    fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,6 @@ import '../assets/css/fullscreen.css'
 const NotFoundPage = () => (
   <div className="full-screen">
     <SEO title="404: Not found" />
-    <video autoPlay muted loop id="YoureLost">
-      <source src="/videos/404.mp4" type="video/mp4" />
-    </video>
     <div className="content404">
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout> 
+    <SEO title="Contact" />
+  </Layout>
+)
+
+export default ContactPage;
